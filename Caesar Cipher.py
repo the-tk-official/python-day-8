daa17428c88a2c65bfd3ffd7047586a4c44ce3ee
the_tk_official <the.tk.official@gmail.com>
2021-10-08 11:15:16 +0300

    Day 8

---
+++ Caesar Cipher.py
@@ -0,0 +1,35 @@
+from art import logo
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caeser(text, shift, direction):
+    end_text = ''
+    if direction == 'decode':
+        shift *= -1
+
+    for char in text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"The {direction}d text is {end_text}")
+
+print(logo)
+
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 25
+    caeser(text=text, shift=shift, direction=direction)
+
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == 'no':
+        should_continue = False
+        print('Goodbye!')
